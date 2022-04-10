4ffa950a42602135a99da525b9f6c0251e3be1c2
wilfried <wilfried.derscanu@assist.ro>
2022-04-10 21:36:58 +0300

    [TLC-23] Register page

--- src/Components/Route/Router.js
+++ src/Components/Route/Router.js
@@ -3,17 +3,19 @@ import Home from "../Home/Home";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "../../StaticComponents/Navbar";
 import Authentication from "../Authentication/Authentication";
+import Register from "../Register/Register";
 const Router = () => {
   const page = window.location.pathname.split("/")[1];
-  console.log(page);
+
   return (
     <div className="page">
       <BrowserRouter>
-        {page !== "auth" && <Navbar />}
+        {page === "auth" || page === "reg" || <Navbar />}
 
         <Routes>
           <Route path="/homepage" element={<Home />} />
           <Route path="/auth" element={<Authentication />} />
+          <Route path="/reg" element={<Register />} />
           <Route path="*" element={<Navigate to="/homepage" />} />
         </Routes>
       </BrowserRouter>
