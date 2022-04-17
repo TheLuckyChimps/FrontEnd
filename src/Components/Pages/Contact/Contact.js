5cd65b122edf3992a438d4b5b8d7c0bf3c7e8fb5
WilfriedStud <84539822+WilfriedStud@users.noreply.github.com>
2022-04-17 20:34:42 +0300

    TPL-26-Contact-page (#7)
    Co-authored-by: wilfried <wilfried.derscanu@assist.ro>

--- src/Components/Pages/Contact/Contact.js
+++ src/Components/Pages/Contact/Contact.js
@@ -7,7 +7,18 @@ const Contact = () => {
     <div className="page">
       <Navbar />
       <Home />
-      <div className="contact">Contact</div>
+      <div className="contact">
+        <h1>Contact TPL Suceava</h1>
+        <br/><br/><br/><br/>
+        <p>Pentru sugestii sau alte ne puteti contacta la:</p>
+        <p>contact@tpl-sv.ro</p>
+        <p>sau</p>
+        <p>Tel/Fax(secretariat): 0330 401 442</p>
+        <br/>
+        <p>SC TRANSPORT PUBLIC LOCAL SA Str.</p>
+        <p>Traian Vuia nr. 5 A, Suceava, 720 021,</p>
+        <p>ROMANIA</p>
+      </div>
     </div>
   );
 };
